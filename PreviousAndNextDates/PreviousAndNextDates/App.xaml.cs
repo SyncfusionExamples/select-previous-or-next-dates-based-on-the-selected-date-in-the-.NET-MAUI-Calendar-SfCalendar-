2ac1a89cd795e36e3f6fb367be0c945917113085
VishalOmprasad <102582824+VishalOmprasad@users.noreply.github.com>
2023-03-08 19:51:36 +0530

    New KB sample added

---
+++ PreviousAndNextDates/PreviousAndNextDates/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PreviousAndNextDates;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ PreviousAndNextDates/PreviousAndNextDates/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PreviousAndNextDates;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
